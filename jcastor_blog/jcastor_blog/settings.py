d262364c8114dc2be8b1d46dc9ae72864470ef8c
Jason Castor <jason@jason-castor.com>
2013-01-06 09:45:23 -0800

    commiting blog app`

--- jcastor_blog/jcastor_blog/settings.py
+++ jcastor_blog/jcastor_blog/settings.py
@@ -120,6 +120,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
     'south',
+	'blog',
 )
 
 # A sample logging configuration. The only tangible logging
