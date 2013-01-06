5b57b74365f790b5736b9d632d23c83d2522398e
Jason Castor <jason@jason-castor.com>
2013-01-06 15:49:33 -0800

    removed settings.py from version control in anticipation of commiting to github, added templates for our models

--- jcastor_blog/blog/models.py
+++ jcastor_blog/blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib import admin
+
+
+class Post(models.Model):
+	title = models.CharField(max_length=60)
+	body = models.TextField()
+	created = models.DateTimeField(auto_now_add=True)
+	
+	def __unicode__(self):
+		return self.title
+
+class PostAdmin(admin.ModelAdmin):
+	search_fields = ["title"]
+
 
 # Create your models here.
---
+++ jcastor_blog/blog/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, include, url
+from django.views.generic import DetailView, ListView, TemplateView
+from blog.models import Post
+urlpatterns = patterns('blog.views',
+	url(r'^$', ListView.as_view(queryset=Post.objects.order_by("-created"), context_object_name="post_list", paginate_by=5,), name="main"),
+	url(r'blog/post/(?P<pk>\d+)/$', DetailView.as_view(model=Post,),name="post_detail_view"),
+	url(r'about/$', TemplateView.as_view(template_name="blog/about.html"), name="about"),
+)
