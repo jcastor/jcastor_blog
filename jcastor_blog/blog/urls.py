8da7a8dd69d239c5f27f18d3503aef3770bf8361
Jason Castor <jason@jason-castor.com>
2013-01-06 19:16:10 -0800

    added in resume, modified urls to include the resume and index to include link to resume

--- jcastor_blog/blog/urls.py
+++ jcastor_blog/blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = patterns('blog.views',
 	url(r'^$', ListView.as_view(queryset=Post.objects.order_by("-created"), context_object_name="post_list", paginate_by=5,), name="main"),
 	url(r'blog/post/(?P<pk>\d+)/$', DetailView.as_view(model=Post,),name="post_detail_view"),
 	url(r'about/$', TemplateView.as_view(template_name="blog/about.html"), name="about"),
+	url(r'resume/$', TemplateView.as_view(template_name="blog/resume.html"), name="resume"),
 )
